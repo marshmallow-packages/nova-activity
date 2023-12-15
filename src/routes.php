ca634149f307ae0ae9d7249dbb7289d249e06dd1
Stef van Esch <stef@marshmallow.dev>
2023-12-15 14:49:15 +0100

    Fixed route issue

--- src/routes.php
+++ src/routes.php
@@ -7,6 +7,6 @@ use Marshmallow\NovaActivity\Http\Controllers\SetQuickReplyController;
 use Marshmallow\NovaActivity\Http\Controllers\CreateActivityController;
 
 Route::post('/{activity_id}/run-action', RunActionController::class);
-Route::post('/{resourceName}/{resourceId}', CreateActivityController::class);
 Route::post('/{activity_id}/set-quick-reply', SetQuickReplyController::class);
+Route::post('/{resourceName}/{resourceId}', CreateActivityController::class);
 Route::get('/{resourceName}/{resourceId}/get-activity', GetActivityController::class);
